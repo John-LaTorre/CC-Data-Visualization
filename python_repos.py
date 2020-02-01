bb06a5f38fab7eb49f7427297ccf04e84aa981ea
John LaTorre <johnjlatorre@gmail.com>
2020-02-01 17:40:58 -0500

    Added tooltips to the python_repos graph with descriptions and clickable links

--- python_repos.py
+++ python_repos.py
@@ -17,11 +17,22 @@ print("Total repositories: ", response_dict['total_count'])
 #Explore information about the repositories
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+        }
+    #Instructions in the book did not account for a repository with no description
+    #If that's the case the chart can not be rendered, this code corrects the issue
+    if repo_dict['description'] is None:
+        plot_dict['label'] = ''
+    ##
+    plot_dicts.append(plot_dict)
 
 #Make vizualization
 my_style = LS('#333366', base_style = LCS)
@@ -40,6 +51,6 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = "Most-Starred Python Projects on GitHub"
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
 
